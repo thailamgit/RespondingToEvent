993093a9fb607642eed5d381c86b909a1f7789b1
Ninh Thai Lam <thailamstudy@gmail.com>
2024-08-08 14:33:08 +0700

    Changing background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-export default function LightSwitch() {
-  function HandleClick() {
-    let bodyStyle = document.body.style
-    if (bodyStyle.backgroundColor === 'black') {
-      bodyStyle.backgroundColor = 'white'
-    } else {
-      bodyStyle.backgroundColor = 'black'
-    }
-  }
+type ButtonProps = {
+  content: string
+  onPress: () => void
+}
 
+// function Button({content, onPress}: ButtonProps) {
+//   <button onClick={onPress}>
+//     {content}
+//   </button>
+// }
+
+function GenerateColor() {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for(let i = 0; i < 6; i++){
+    color = color + letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
+export default function BackgroundChange() {
+  function handleClick() {
+    let bodyStyle = document.body.style
+    bodyStyle.backgroundColor = GenerateColor()
+  }
   return (
-    <button onClick={HandleClick}>
-      Toggle the lights
+    <button onClick={handleClick}>
+      Change color
     </button>
   )
 }
