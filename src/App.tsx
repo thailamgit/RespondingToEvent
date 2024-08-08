b82b78f97d345b231c005f40368cd13cf80e82a8
Ninh Thai Lam <thailamstudy@gmail.com>
2024-08-08 10:41:57 +0700

    Light Toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,16 @@
+export default function LightSwitch() {
+  function HandleClick() {
+    let bodyStyle = document.body.style
+    if (bodyStyle.backgroundColor === 'black') {
+      bodyStyle.backgroundColor = 'white'
+    } else {
+      bodyStyle.backgroundColor = 'black'
+    }
+  }
 
-export default function App() {
   return (
-    <Toolbar
-      onPlayMovie={() => alert('Playing!')}
-      onUpLoadImage={() => alert('Uploading!')}
-    />
-  )
-}
-
-type ButtonProps = {
-  onClick: () => void
-  children: string
-}
-
-function Button ({onClick, children}: ButtonProps){
-  return(
-    <button onClick={onClick}>
-      {children}
+    <button onClick={HandleClick}>
+      Toggle the lights
     </button>
-  )
-}
-
-type ToolbarProps = {
-  onPlayMovie: () => void
-  onUpLoadImage: () => void
-}
-function Toolbar ({onPlayMovie, onUpLoadImage}:ToolbarProps){
-  return (
-    <div>
-      <Button onClick={onPlayMovie}>
-        Play movie
-      </Button>
-      <Button onClick={onUpLoadImage}>
-        Upload image
-      </Button>
-    </div>
   )
 }
